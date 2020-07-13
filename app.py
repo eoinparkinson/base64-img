635e16de9bb99dd6a501d88ffbb9bcf238ea59b4
eoinparkinson <eoinparkinson@users.noreply.github.com>
2020-07-13 01:03:32 +0100

    First step to adding app graphing

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+#Basic image url to base64 (working)
 @app.route("/api/v1/img-base64")
 def home():
     if 'url' in request.args:
@@ -15,6 +16,21 @@ def home():
             return "Error: Invalid url. Url must be a direct link to image."
     else:
         return "Error: No url field provided. Please specify a url using ?url=www.your-url-here.com"
+
+#Taking in two lists, and outputting a graph in image base64 format.
+@app.route("/api/v1/graph-img")
+def graphImage():
+    if "xVal" and "yVal" in request.args:
+        try:
+            tmpXVal = str(request.args['xVal'])
+            tmpYVal = str(request.args['yVal'])
+            return ("xVal = " + tmpXVal + " and yVal = " + tmpYVal)
+        except:
+            return "Error: This is the 'except' issue."
+    else:
+        return "Error: No parameters provided."
+
+
 
 
 
