a71e16f2cf300ef986981239f2d0b52eff776cd0
eoinparkinson <eoinparkinson@users.noreply.github.com>
2020-07-15 00:45:12 +0100

    Fixed /api/v1 and created /api/v2 for testing purposes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import base64
 import requests
 from flask import Flask, request
 import time
-from urllib.parse import urlparse
+import urllib.parse
 
 app = Flask(__name__)
 
@@ -18,6 +18,24 @@ def home():
             return "Error: Invalid url. Url must be a direct link to image."
     else:
         return "Error: No url field provided. Please specify a url using ?url=www.your-url-here.com"
+
+#--------------------------------------------------------------------------
+
+@app.route("/api/v2/img-base64")
+def secondAttempt():
+    if 'url' in request.args:
+        try:
+            #query = str(request.args['url'])
+            query = urllib.parse.quote_plus(str(request.args['url']))
+            return base64.b64encode(requests.get(query).content)
+        except:
+            return "Error: Invalid url. Url must be a direct link to image."
+    else:
+        return "Error: No url field provided. Please specify a url using ?url=www.your-url-here.com"
+
+
+#--------------------------------------------------------------------------
+
 
 #Taking in two lists, and outputting a graph in image base64 format.(in progress)
 @app.route("/api/v1/graph-img")
